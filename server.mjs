be974b26279d7841ce8ff23b5b00b5fc426bd849
Syed Kariz Ali <karizali310@gmail.com>
2022-12-03 12:50:19 +0500

    weather app

--- server.mjs
+++ server.mjs
@@ -2,9 +2,19 @@ import express from 'express';
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/profile', (req, res) => {
+    console.log("requested ip: ", req.ip);
+  res.send('profile!')
 })
+
+app.get('/about', (req, res) => {
+    console.log("requested ip: ", req.ip);
+    res.send('about!')
+})
+// app.use('/', express.static())
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
